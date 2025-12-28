f6de8a82112a0f16b699fbe6b7a89daa6dd7c824
Madhuranga Senadheera <lilan.maduranga@gmail.com>
2025-12-28 22:48:46 +0400

    added sample env files

--- backend/src/middlewares/delay.middleware.ts
+++ backend/src/middlewares/delay.middleware.ts
@@ -1,4 +1,4 @@
-export function delayMiddleware(ms = 2000) {
+export function delayMiddleware(ms = 1000) {
   return async (ctx: any, next: () => Promise<void>) => {
     if (process.env.NODE_ENV === "development") {
       await new Promise(resolve => setTimeout(resolve, ms));
